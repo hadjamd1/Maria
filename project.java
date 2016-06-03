5e1c9d7d940cd36a89e0b72b24caf801a0cde2b0
hadjamd1 <hadja.md@bronxsoftware.org>
2016-06-03 08:56:13 -0400

    Add files via upload

---
+++ project.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class project {
+
+  public static void main(String[] args) {
+Scanner keyboard = new Scanner(System.in);
+
+System.out.println("Hit Enter");
+
+String name = keyboard.nextLine();
+
+if ( name != null ) {
+
+System.out.println("Every posts saves itself and it becomes a memory that would not delete unless you do so yours"); }
+
+}
+}
